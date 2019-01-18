267032b7688ff12784cf16a61b99f95d3bc6fe03
cerberus <cerberusprogrammer@gmail.com>
2019-01-18 12:14:02 -0800

    Now works the text fields.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,10 +10,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
+import javax.swing.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -43,28 +45,34 @@ public class Controller implements Initializable {
     private TextField textfieldKilogramos;
 
     @FXML
-    void changeValue(MouseEvent event) {
-        System.out.println("drag");
-        System.out.println(((Slider) event.getSource()).getValue());
-    }
-
-    @FXML
     void putData(KeyEvent event) {
-        System.out.println("put");
+        if (event.getCode() == KeyCode.ENTER) {
+            if (textfieldKilogramos.getText().matches("[a-zA-Z]+")
+                    || textfieldKilogramos.getText().matches("[0-9]+[a-zA-Z]+")
+                    || textfieldKilogramos.getText().matches("[a-zA-Z]+[0-9]+")) {
+                JOptionPane.showMessageDialog(null, "Solo se admiten digitos.");
+                textfieldKilogramos.setText("");
+            } else {
+                sliderKilogramos.setValue(Double.parseDouble(textfieldKilogramos.getText()));
+                // falta colocar slider libras
+                labelKilogramos.setText(textfieldKilogramos.getText() + " kg");
+            }
+        }
     }
 
     @FXML
     void saveData(ActionEvent event) {
-        System.out.println("save");
+
     }
 
     @FXML
     void viewData(ActionEvent event) {
-        System.out.println("view");
+
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        // Falta mover los sliders al desplazar uno
         sliderKilogramos.valueProperty().addListener((observable, oldValue, newValue) -> {
             labelKilogramos.setText("Kilogramos: " + (float) sliderKilogramos.getValue() + " kg");
             labelLibras.setText("Libras: " + (float) (sliderKilogramos.getValue() * 2.20462) + " lb");
